dba41a5cdb2c37ba93678ce5c5fa1ac0aa882619
Prashant Verma <vprashant4718@gmail.com>
2025-10-07 14:35:11 +0530

    backend chagnes  and founder image new

--- Backend/index.js
+++ Backend/index.js
@@ -1,35 +1,37 @@
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-
 const app = express();
-app.use(cors());
+app.use(cors()); // allow all origins
 app.use(express.json());
 
 app.post("/proxy", async (req, res) => {
   const googleURL = process.env.VITE_GOOGLESHEETURL;
+
   try {
     const response = await fetch(googleURL, {
-      method: "POST",   
+      method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body),
     });
 
-    // Try to parse JSON; if not JSON, just return raw text
     const text = await response.text();
     try {
       const json = JSON.parse(text);
-       res.json(json);
+      res.json(json);
     } catch {
-      res.status(200).send(text);
+      res.send(text);
     }
-
   } catch (err) {
     res.status(500).json({ error: "Proxy failed", details: err.message });
   }
 });
 
-app.listen(3000, () => console.log("SERVER running on 3000"));
+app.get("/", (req, res) => {
+  res.send("Hi optifusion");
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
